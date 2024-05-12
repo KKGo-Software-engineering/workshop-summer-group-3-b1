c60bf323d342602f7e9eefbd86252d208b7b54eb
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-05-12 10:27:09 +0700

    add get spender intergration test

--- api/spender/spender_it_test.go
+++ api/spender/spender_it_test.go
@@ -4,6 +4,8 @@ package spender
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -65,6 +67,50 @@ func TestGetAllSpenderIT(t *testing.T) {
 
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.NotEmpty(t, rec.Body.String())
+	})
+}
+
+func TestGetSpenderByIDIT(t *testing.T) {
+	t.Run("get spender by id successfully", func(t *testing.T) {
+		sql, err := getTestDatabaseFromConfig()
+		if err != nil {
+			t.Error(err)
+		}
+		migration.ApplyMigrations(sql)
+		defer migration.RollbackMigrations(sql)
+
+		h := New(config.FeatureFlag{EnableCreateSpender: true}, sql)
+		e := echo.New()
+		defer e.Close()
+
+		e.POST("/spenders", h.Create)
+		e.GET("/spenders/:id", h.GetByID)
+
+		var sp Spender
+		{
+			payload := `{"name": "HongJot", "email": "hong@jot.ok"}`
+			req := httptest.NewRequest(http.MethodPost, "/spenders", strings.NewReader(payload))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+
+			e.ServeHTTP(rec, req)
+			assert.Equal(t, http.StatusCreated, rec.Code)
+			assert.NotEmpty(t, rec.Body.String())
+
+			if err := json.NewDecoder(rec.Result().Body).Decode(&sp); err != nil {
+				assert.NoError(t, err)
+			}
+		}
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/spenders/%d", sp.ID), nil)
+		rec := httptest.NewRecorder()
+
+		e.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.NotEmpty(t, rec.Body.String())
+		assert.Equal(t, sp.Name, "HongJot")
+		assert.Equal(t, sp.Email, "hong@jot.ok")
 	})
 }
 
